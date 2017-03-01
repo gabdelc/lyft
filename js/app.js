540a1072a94d1905c21889442486a6728a7112e1
gabriela <gabdelc@gmail.com>
2017-03-01 12:57:07 -0500

    barra de autos map html

--- js/app.js
+++ js/app.js
@@ -18,15 +18,17 @@ var pazPeru = new google.maps.Marker({
     position: myLatLng,
     map: map,
     title: 'aqui estoy',
-    icon: "img/person_3_30x40.png"
+    icon: " "
     });
+    
+    
 
 var myLatLng2 = {lat: -16.46134919999998, lng: -71.5395308};
 var cerca1 = new google.maps.Marker({
     position: myLatLng2,
     map: map,
     title: 'carro1',
-    icon: "img/car_30x40.jpg"
+    icon: " "
     });
 
 var myLatLng3 = {lat: -16.45538919999998, lng: -71.5455308};
@@ -34,7 +36,7 @@ var cerca2 = new google.maps.Marker({
     position: myLatLng3,
     map: map,
     title: 'carro2',
-    icon: "img/car_30x40.jpg"
+    icon: " "
     });
 
 var myLatLng4 = {lat: -16.45938919999998, lng: -71.5295308};
@@ -42,6 +44,6 @@ var cerca3 = new google.maps.Marker({
     position: myLatLng4,
     map: map,
     title: 'carro3',
-    icon: "img/car_30x40.jpg"
+    icon: " "
     });
 }
